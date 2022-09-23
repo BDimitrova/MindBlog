74377949777aca28b6cab51b40adf7c228ef2851
Boryana Dimitrova <b.dimitrova97@gmail.com>
2022-09-23 18:47:53 +0300

    Make router.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const { PORT } = require('./constans');
+const router = require('./router');
 
 const app = express();
+
 
 require('./config/hbsConfig')(app);
 require('./config/expressConfig')(app);
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use(router);
 
 app.listen(PORT, () => console.log(`The app is running on port http://localhost:${PORT}....`));
